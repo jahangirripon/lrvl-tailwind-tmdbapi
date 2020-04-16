ba0f7eb0c650e1109113d22b8e9f35585f5877e5
jahangirripon <jripon1991@gmail.com>
2020-04-16 11:33:59 +0600

    refactoring to view model with spatie package

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -4,14 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use App\ViewModels\MoviesViewModel;
+use App\ViewModels\MovieViewModel;
 
 class MoviesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $popularMovies = Http::withToken(config('services.tmdb.token'))
@@ -22,50 +19,38 @@ class MoviesController extends Controller
             ->get('https://api.themoviedb.org/3/movie/now_playing?api_key=f58db137115b5ace10ffab5475c4d2fb')
             ->json()['results'];
 
-        $genresArray = Http::get('https://api.themoviedb.org/3/genre/movie/list?api_key=f58db137115b5ace10ffab5475c4d2fb')
+        $genres = Http::get('https://api.themoviedb.org/3/genre/movie/list?api_key=f58db137115b5ace10ffab5475c4d2fb')
             ->json()['genres'];
 
-        $genres = collect($genresArray)->mapWithKeys(function($genre) {
-            return [$genre['id'] => $genre['name']];
-        });
+        // $genres = collect($genresArray)->mapWithKeys(function($genre) {
+        //     return [$genre['id'] => $genre['name']];
+        // });
 
+        // return view('index', [
+        //     'popularMovies' => $popularMovies,
+        //     'nowPlayingMovies' => $nowPlayingMovies,
+        //     'genres' => $genres
+        // ]);
 
+        $viewModel = new MoviesViewModel(
+            $popularMovies,
+            $nowPlayingMovies,
+            $genres
+        );
 
-        
-        return view('index', [
-            'popularMovies' => $popularMovies,
-            'nowPlayingMovies' => $nowPlayingMovies,
-            'genres' => $genres
-        ]);
+        return view('index', $viewModel );
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //https://api.themoviedb.org/3/movie/481848?append_to_response=credits,videos,images&api_key=f58db137115b5ace10ffab5475c4d2fb
@@ -75,40 +60,21 @@ class MoviesController extends Controller
 
         // dump($movie);
 
-        return view('show', [
-            'movie' => $movie
-        ]);
+        $viewModel = new MovieViewModel($movie);
+
+        return view('show', $viewModel);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
